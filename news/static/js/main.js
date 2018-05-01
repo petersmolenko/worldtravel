dbefcc65dc44366d64367c760cdee280b7135db7
Peter Smolenko <pi.smolenko@gmail.com>
2018-05-01 16:51:12 +0300

    v .5

---
+++ news/static/js/main.js
@@ -0,0 +1,179 @@
+(function () {
+	
+	'use strict';
+
+
+
+	// iPad and iPod detection	
+	var isiPad = function(){
+		return (navigator.platform.indexOf("iPad") != -1);
+	};
+
+	var isiPhone = function(){
+	    return (
+			(navigator.platform.indexOf("iPhone") != -1) || 
+			(navigator.platform.indexOf("iPod") != -1)
+	    );
+	};
+
+	// Main Menu Superfish
+	var mainMenu = function() {
+
+		$('#primary-menu').superfish({
+			delay: 0,
+			animation: {
+				opacity: 'show'
+			},
+			speed: 'fast',
+			cssArrows: true,
+			disableHI: true
+		});
+
+	};
+
+	//Date Picker
+
+   $('#date-start, #date-end, #date-birthday').datepicker();
+
+   [].slice.call( document.querySelectorAll( 'select.cs-select' ) ).forEach( function(el) {  
+      new SelectFx(el);
+   } );
+
+	// Parallax
+	var parallax = function() {
+		if ( !isiPad() || !isiPhone() ) {
+			$(window).stellar();
+		}
+	};
+
+
+	// Offcanvas and cloning of the main menu
+	var offcanvas = function() {
+
+		var $clone = $('#menu-wrap').clone();
+		$clone.attr({
+			'id' : 'offcanvas-menu'
+		});
+		$clone.find('> ul').attr({
+			'class' : '',
+			'id' : ''
+		});
+
+		$('#page').prepend($clone);
+
+		// click the burger
+		$('.js-nav-toggle').on('click', function(){
+
+			if ( $('body').hasClass('offcanvas') ) {
+				$('body').removeClass('offcanvas');
+			} else {
+				$('body').addClass('offcanvas');
+			}
+			// event.preventDefault();
+
+		});
+
+		$('#offcanvas-menu').css('height', $(window).height());
+
+		$(window).resize(function(){
+			var w = $(window);
+
+
+			$('#offcanvas-menu').css('height', w.height());
+
+			if ( w.width() > 769 ) {
+				if ( $('body').hasClass('offcanvas') ) {
+					$('body').removeClass('offcanvas');
+				}
+			}
+
+		});	
+
+	}
+	// Click outside of the Mobile Menu
+	var mobileMenuOutsideClick = function() {
+		$(document).click(function (e) {
+	    var container = $("#offcanvas-menu, .js-nav-toggle");
+	    if (!container.is(e.target) && container.has(e.target).length === 0) {
+	      if ( $('body').hasClass('offcanvas') ) {
+				$('body').removeClass('offcanvas');
+			}
+	    }
+		});
+	};
+
+
+	// Animations
+
+	var contentWayPoint = function() {
+		var i = 0;
+		$('.animate-box').waypoint( function( direction ) {
+
+			if( direction === 'down' && !$(this.element).hasClass('animated') ) {
+				
+				i++;
+
+				$(this.element).addClass('item-animate');
+				setTimeout(function(){
+
+					$('body .animate-box.item-animate').each(function(k){
+						var el = $(this);
+						setTimeout( function () {
+							el.addClass('fadeInUp animated');
+							el.removeClass('item-animate');
+						},  k * 50, 'easeInOutExpo' );
+					});
+					
+				}, 100);
+				
+			}
+
+		} , { offset: '85%' } );
+	};
+	
+	var stickyBanner = function() {
+		var $stickyElement = $('.sticky-banner');
+		var sticky;
+		if ($stickyElement.length) {
+		  sticky = new Waypoint.Sticky({
+		      element: $stickyElement[0],
+		      offset: 0
+		  })
+		}
+	}; 
+	// Document on load.
+	$(function(){
+		mainMenu();
+		parallax();
+		offcanvas();
+		mobileMenuOutsideClick();
+		contentWayPoint();
+		stickyBanner();
+	});
+	
+	//-------------Script Background for Tours Cards---------------/
+	function changeBack(){
+		var list = document.querySelectorAll("div[data-image]");
+		for (var i = 0; i < list.length; i++) {
+		  var url = list[i].getAttribute('data-image');
+			list[i].style.backgroundImage="url('" + url + "')";
+			list[i].style.backgroundPosition="center center";
+			list[i].style.backgroundSize="cover";
+			list[i].style.backgroundRepeat="no-repeat";
+		}
+	}
+	
+	if(document.documentElement.clientWidth < 770) {
+		changeBack();
+  	}
+	$(window).resize(function() {
+		if(document.documentElement.clientWidth < 770) {
+			changeBack();
+	  } else {
+		   $(".tour-container").css({
+			'background': '#fff',
+		});
+	  }
+	});
+	//-----------------------------------------------
+}());
