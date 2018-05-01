aa9862394ce39261eb19b720b2921551b74c917f
Peter Smolenko <pi.smolenko@gmail.com>
2018-05-01 15:12:54 +0300

    v .4

--- news/models.py
+++ news/models.py
@@ -5,6 +5,7 @@ from django.utils import timezone
 class Post(models.Model):
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
+    short_descript = models.CharField(max_length=300)
     text = models.TextField()
     created_date = models.DateTimeField(
             default=timezone.now)
