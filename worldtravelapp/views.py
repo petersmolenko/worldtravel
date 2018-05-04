f04d55a84b0db6c676847b281ce5490973f7d855
Peter Smolenko <pi.smolenko@gmail.com>
2018-05-04 23:50:49 +0300

    v .992

--- worldtravelapp/views.py
+++ worldtravelapp/views.py
@@ -6,8 +6,19 @@ def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'news/news.html', {'posts': posts})
 
+def admin_news(request):
+    return render(request, 'admin/admin_news.html', {})
+
+def admin_tours(request):
+    return render(request, 'admin/admin_tours.html', {})
+
+def admin_orders(request):
+    return render(request, 'admin/admin_orders.html', {})
+
+def admin_settings(request):
+    return render(request, 'admin/admin_settings.html', {})
+
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.tags = post.tags.split(',')
-    print(post.short_descript)
     return render(request, 'news/news_detail.html', {'post': post})
