5d162af861780aedee4b764427e32a4f36cfe7d1
Peter Smolenko <pi.smolenko@gmail.com>
2018-05-06 02:53:03 +0300

    v .992

--- worldtravelapp/forms.py
+++ worldtravelapp/forms.py
@@ -6,3 +6,10 @@ class PostForm(forms.ModelForm):
 	class Meta:
 		model = Post
 		fields = ('title', 'short_descript', 'text', 'tags', 'photo')
+		widgets = {
+        'title' : forms.TextInput(attrs={'placeholder' : 'Заголовок', 'class' : 'form-control'}),
+        'short_descript' : forms.Textarea(attrs={'placeholder' : 'Краткое описание', 'rows' : '7', 'class' : 'form-control'}),
+        'text' : forms.Textarea(attrs={'placeholder' : 'Текст статьи', 'rows' : '15', 'class' : 'form-control'}),
+        'tags' : forms.TextInput(attrs={'placeholder' : 'тэг1, тэг2, ..', 'class' : 'form-control'}),
+        'photo' : forms.FileInput(attrs={'class' : 'custom-file-input', 'lang' : 'ru'}),
+    }
