1030b21b9793746e21e5c248ac360fc79f1e23f0
Peter Smolenko <pi.smolenko@gmail.com>
2018-05-08 17:42:38 +0300

    v .99285

--- worldtravelapp/views.py
+++ worldtravelapp/views.py
@@ -32,15 +32,35 @@ def sign_up(request):
 
 @login_required(login_url='/auth/sign-in/')
 def admin_news(request):
-	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
-	return render(request, 'admin/admin_news.html', {'posts': posts})
+    return redirect('admin_news_ok')
 
+@login_required(login_url='/auth/sign-in/')
+def admin_news_ok(request):
+	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+	return render(request, 'admin/admin_news_ok.html', {'posts': posts})
+
+@login_required(login_url='/auth/sign-in/')
+def admin_news_draft(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request, 'admin/admin_news_draft.html', {'posts': posts})
+
+@login_required(login_url='/auth/sign-in/')
 def admin_tours(request):
     return render(request, 'admin/admin_tours.html', {})
-
+    
+@login_required(login_url='/auth/sign-in/')
+def admin_mytours(request):
+    return render(request, 'admin/admin_mytours.html', {})
+    
+@login_required(login_url='/auth/sign-in/')
+def admin_comments(request):
+    return render(request, 'admin/admin_comments.html', {})
+    
+@login_required(login_url='/auth/sign-in/')
 def admin_orders(request):
     return render(request, 'admin/admin_orders.html', {})
-
+    
+@login_required(login_url='/auth/sign-in/')
 def admin_settings(request):
     return render(request, 'admin/admin_settings.html', {})
 
@@ -59,13 +79,22 @@ def post_new(request):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
             post.save()
-            return redirect('post_detail', pk=post.pk)
+            return redirect('admin_news_draft', pk=post.pk)
     else:
         form = PostForm()
     return render(request, 'news/news_edit.html', {'form': form})
-  
+
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('admin_news', pk=pk)
+
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('admin_news')
+
 @login_required(login_url='/auth/sign-in/')
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
@@ -74,7 +103,6 @@ def post_edit(request, pk):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
             post.save()
             return redirect('admin_news')
     else:
