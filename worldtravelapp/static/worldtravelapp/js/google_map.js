714ab2ee96246fe0bad2c5ca883b0d229c8a8ac3
Peter Smolenko <pi.smolenko@gmail.com>
2018-05-11 12:36:29 +0300

    v .99299

--- worldtravelapp/static/worldtravelapp/js/google_map.js
+++ worldtravelapp/static/worldtravelapp/js/google_map.js
@@ -36,10 +36,11 @@ function init() {
         $.getJSON('http://maps.googleapis.com/maps/api/geocode/json?address='+addresses[x]+'&sensor=false', null, function (data) {
             var p = data.results[0].geometry.location
             var latlng = new google.maps.LatLng(p.lat, p.lng);
+            var hiddy = document.getElementById('hiddy').innerHTML
             new google.maps.Marker({
                 position: latlng,
                 map: map,
-                icon: 'images/loc.png'
+                icon: hiddy
             });
 
         });
